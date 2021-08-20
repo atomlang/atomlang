3ad814d837918100608bebfbcd4c0b7224599f0b
krishpranav <krisna.pranav@gmail.com>
2021-08-20 15:53:12 +0530

    tests: benchmarks list

---
+++ tests/benchmarks/list/list.js
@@ -0,0 +1,22 @@
+function reverse(list) {
+	var i=0, tmp, idx;
+	var count = list.length;
+	var max = count / 2 | 0;
+
+	for (; i < max; i++) {
+		idx = count - i - 1;
+		tmp = list[idx];
+		list[idx] = list[i];
+		list[i] = tmp;
+	}
+}
+
+var start = process.hrtime();
+
+var i=0, N=20000000, list=Array(N);
+for (; i < N; i++) list[i] = i;
+reverse(list);
+
+var end = process.hrtime(start);
+var secs = (end[0] + end[1] / 1e9).toFixed(6) + 's';
+console.log('elapsed:', secs);
