6f132eadac34cfcfdda0f1a9d425bae50cfe298b
krishpranav <krisna.pranav@gmail.com>
2021-08-20 15:48:11 +0530

    tests: benchmarks factors js, lua

---
+++ tests/benchmarks/factors/factors.js
@@ -0,0 +1,11 @@
+var start = process.hrtime();
+
+var N = 50000000;
+var i=0, factors = []
+for (; i <= N; i++) {
+	if (N % i === 0) factors.push(i);
+}
+
+var end = process.hrtime(start);
+var secs = (end[0] + end[1] / 1e9).toFixed(6) + 's';
+console.log('elapsed:', secs);
