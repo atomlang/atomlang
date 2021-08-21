25940df429dd7293f246825f3bbd45c36285fd31
krishpranav <krisna.pranav@gmail.com>
2021-08-21 11:31:57 +0530

    cli: internal
    internal header cli

---
+++ cli/internal.h
@@ -0,0 +1,24 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+// includes
+#include <pocketlang.h>
+#include <assert.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <stdio.h>
+
+#include "common.h"
+
+#define CLI_NOTICE                                                            \
+  "AtomLang " ATML_VERSION_STRING " (https://github.com/atomlang/atomlang/)\n" \
+  "Copyright(c) 2020 - 2021 AtomLangDevelopers.\n"                                 \
+  "Free and open source software under the terms of the MIT license.\n"
+
+typedef struct {
+  bool repl_mode;
+} VmUserData;
+
+#endif
