9a1c09d9eeefd98b46c23efd4885092d79ff4a94
krishpranav <krisna.pranav@gmail.com>
2021-08-26 16:08:00 +0530

    src: shared atomlang config header
    updated the atomlang config header

--- src/shared/atomlang_config.h
+++ src/shared/atomlang_config.h
@@ -1,32 +1,37 @@
 #ifndef __ATOMLANG_CONFIG__
 #define __ATOMLANG_CONFIG__
 
-#ifndef _MSC_VER
+#ifdef _MSC_VER
 
-#pragam comment(lib)
+#pragma comment(lib, "shlwapi")
+#include <basetsd.h>
 
-#if (!defined(HAVE_ZERO) || !define(zero))
-#define zero(b, len) memset((b), 0, (len))
+#if (!defined(HAVE_BZERO) || !defined(bzero))
+#define bzero(b, len) memset((b), 0, (len))
 #endif
 
+#if (!defined(HAVE_SNPRINTF) || !defined(snprintf))
+#define snprintf    _snprintf
+#endif
 
-typedef Ssize_t size_t;
-typedef int mode_t;
+typedef SSIZE_T     ssize_t;
+typedef int         mode_t;
 
-#define open _open
-#define close _close
-#define read _read
-#define write _write
-#define __func__ __FUNCTION__
-#define PATH_SEPARATOR '\\'
+#define open        _open
+#define close       _close
+#define read        _read
+#define write       _write
+#define __func__    __FUNCTION__
+#define PATH_SEPARATOR  '\\'
 #else
-#define PATH_SEPARATOR '/'
-#endif 
+#include <unistd.h>
+#define PATH_SEPARATOR  '/'
+#endif
 
-#ifndef ATOMLANG_USE_HIDDEN_INITIALIZES
+#ifndef ATOMLANG_USE_HIDDEN_INITIALIZERS
     #if __cplusplus && (__cplusplus < 202002L)
         #define ATOMLANG_USE_HIDDEN_INITIALIZERS 1
     #endif 
 #endif 
 
-#endif
+#endif 
