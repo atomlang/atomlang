a333df5d23c9127b5fbab17a2f478e5b82a7fbfc
krishpranav <krisna.pranav@gmail.com>
2021-08-26 15:55:44 +0530

    cli: atomlang

--- cli/thirdparty.c
+++
@@ -1,8 +0,0 @@
-/*
- *  Copyright (c) 2020-2021 AtomLanguage Developers
- *  Distributed Under The MIT License
- */
-
-#include "thirdparty/cwalk/cwalk.c"
-
-#include "thirdparty/argparse/argparse.c"
