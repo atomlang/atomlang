c5bfa5c86f000acbc8447fbe1acdf84e699edd21
krishpranav <krisna.pranav@gmail.com>
2021-08-28 12:09:00 +0530

    src: compiler atomlang semacheck2 header

---
+++ src/compiler/atomlang_semacheck2.h
@@ -0,0 +1,9 @@
+#ifndef __ATOMLANG_SEMACHECK2__
+#define __ATOMLANG_SEMACHECK2__
+
+#include "atomlang_ast.h"
+#include "atomlang_delegate.h"
+
+bool atomlang_semacheck2(gnode_t *node, atomlang_delegate_t *delegate);
+
+#endif
