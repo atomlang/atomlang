32ba3f2a41a12b0a818c0b09cc832c36d5e96db7
krishpranav <krisna.pranav@gmail.com>
2021-09-04 13:36:33 +0530

    update

--- src/compiler/atomlang_optimizer.h
+++
@@ -1,9 +0,0 @@
-#ifndef __ATOMLANG_OPTIMIZER__
-#define __ATOMLANG_OPTIMIZER__
-
-#include "atomlang_macros.h"
-#include "atomlang_value.h"
-
-atomalang_function_t *atomlang_optimizer(atomlang_function_t *f, bool add_debug);
-
-#endif
--- src/compiler/atomlang_semacheck1.h
+++
@@ -1,9 +0,0 @@
-#ifndef __ATOMLANG_SEMACHECK1__
-#define __ATOMLANG_SEMACHECK1__
-
-#include "atomlang_ast.h"
-#include "atomlang_delegate.h"
-
-bool atomlang_semacheck1(gnode_t *node, atomlang_delegate_t *delegate);
-
-#endif
